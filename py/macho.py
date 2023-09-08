cfefb771e7ed813fb88066cd64d7d571118d0e10
Esther Alter <subalterngames@gmail.com>
2023-09-08 15:16:34 -0400

    directory

--- py/macho.py
+++ py/macho.py
@@ -2,9 +2,10 @@ from fat_macho import FatWriter
 
 
 writer = FatWriter()
-with open("../target/aarch64-apple-darwin/release/libfast_image_encoder.dylib", "rb") as f:
+root_dir = "../rs/fast_image_encoder/target/"
+with open(f"{root_dir}aarch64-apple-darwin/release/libfast_image_encoder.dylib", "rb") as f:
     writer.add(f.read())
-with open("../target/x86_64-apple-darwin/release/libfast_image_encoder.dylib", "rb") as f:
+with open(f"{root_dir}x86_64-apple-darwin/release/libfast_image_encoder.dylib", "rb") as f:
     writer.add(f.read())
 # Get Mach-O fat binary as bytes
 fat_bytes = writer.generate()
